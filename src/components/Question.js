fbdf44893ebf252d3012f6f9eeae47cda1ac3ac9
Matthew Bomett <bomettmat2@gmail.com>
2023-10-31 19:51:59 +0300

    completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  let timer = null
 
   // add useEffect code
+  useEffect(() => {
+    // set a timeout to run a callback function after 1s
+    timer = setTimeout(() =>{
+      // if there's still time remaining, decrease it by 1s
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1)
+      } else {
+        // if no time is remaining, reset the timer and call onAnswered with false
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+
+    // cleanup function: clear the timeout when the component unmounts or before starting a new timeout
+    return () => clearTimeout(timer)
+  }, [timeRemaining, onAnswered]) // dependencies: re-run the useEffect when these values change
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
